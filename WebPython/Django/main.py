e8b253d69b912b005dc458dc4c70a829a73260c8
Leonardo Wons <leonardowons@gmail.com>
2023-03-27 17:33:52 -0300

    27-03-23 curso 1 django

---
+++ WebPython/Django/main.py
@@ -0,0 +1,36 @@
+# Curso um
+# Primeira aula
+"""
+    Primeira aula foi a aula padrao, configuração, instalação, msm coisa de sempre
+"""
+
+# Segunda aula
+"""
+    Segunda aula não teve nada de novo/legal, foi apenas como configurar no settings e como o .evn funciona
+    Tambem foi mostrado como utilizar o gitignore e comandos git
+"""
+
+# Terceira aula
+"""
+    Começou o coding do site/app, como criar um app
+    Foi mostrado como deixar os pathings dos apps dentro dos apps, não no setup
+"""
+
+# Quarta aula
+"""
+    foi mostrado como usar/configurar os static files
+"""
+
+# Quinta aula
+"""
+    Foi mostrado como dar nomes para URL e chama-las no HTML
+    DRY, Don't Reapet Yourself, como nao repetir codigos/template
+    Utilizando os block content e endblock e tambem partials, utilizando o include
+"""
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# Curso dois
+# Primeira aula
+"""
+    
+"""
