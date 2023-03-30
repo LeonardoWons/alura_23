bd9cceeebb33d9d3173690edc327365f94eb7584
Leonardo Wons <leonardowons@gmail.com>
2023-03-30 15:59:59 -0300

    curso um django-api 30-03-23 complete

---
+++ WebPython/Django-api/main.py
@@ -0,0 +1,37 @@
+# Curso um
+# Primeira aula
+"""
+    começamos a fazer a configuração do django rest framework
+"""
+
+# Segunda aula e terceira aula
+"""
+    Criamos um modelo chamado Aluno e outro Curso
+    Criamos o serializer.py que vai ser responsavel de fazer a ponto PY para JSON e JSON para PY
+    Com o serializer utilizamos o pacote routes para exibir os valores 
+"""
+
+# Quarta aula
+"""
+    Ja tinhamos o metodo GET e POST, agora aprendemos o PUT e PATCH
+    PUT = atualização de TODOS os dados
+    PATCH = atualização de um dado
+    e para finalizar foi mostrado o metodo DELETE
+    
+    Integramos as matriculas com os cursos e alunos dos outros banco de dados(class Aluno e Curso)
+"""
+
+# Quinta aula
+"""
+    customizamos um serializer para trazer a lista de matriculas de um aluno e lista de alunos matriculados em um curso
+    
+    Fizemos a parte de autenticação
+"""
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# Curso dois
+# Primeira aula
+"""
+    
+"""
+
